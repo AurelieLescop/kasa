8a859da84b9e17f4d333e5f35de3ed7ead495196
Aurelie Lescop <lescop.aurelie@live.fr>
2023-09-14 12:32:14 +0200

    creation dossier css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
-//import logo from './logo.svg';
-import "./App.css";
+//import logo from './logo.svg'; => penser à supprimer ce logo dans dossier
+import "./css/app.css";
 
 import Home from "./pages/Home";
 import Accomodation from "./pages/Accomodation";
@@ -10,24 +10,7 @@ import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    /*<div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div> */
-
-    <Routes>
+        <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/Accomodation" element={<Accomodation />} />
       <Route path="/AboutUs" element={<AboutUs />} />
