54771eb8580bf1b1610a101941d621a008fed5b8
Aurelie Lescop <lescop.aurelie@live.fr>
2023-09-28 16:07:10 +0200

    finalisation bannière page home

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,9 +3,11 @@ import coverpicture from '../IMG_cover.png';
 function Banner() {
     return (
 <div className='banner'>
+<h1>Chez vous, <br /> partout et ailleurs</h1>
 <img src={coverpicture} className="banner_img" alt="photographie de paysage" /> 
+
 </div>
     );
-  }
+  };
   
   export default Banner;
