6c4f7402c4f77e98c655b1ac1e7bb83c1240026b
Aurelie Lescop <lescop.aurelie@live.fr>
2023-10-05 14:38:58 +0200

    poursuite collapse page about

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { useState } from "react";
 
 // function Collapse(){
@@ -14,24 +14,29 @@ import { useState } from "react";
 // }
 // export default Collapse;
 
-function Collapse(props){
-    const [open, setOPen] = useState(true);
-    // à changer selon si on veut ouvert ou non
+function Collapse(props) {
+  const [open, setOPen] = useState(true);
+  // à changer selon si on veut ouvert ou non
 
-    const toggle = () => {
-        setOPen(!open);
-      };
-      
-      return (
-        <div>
-          <button onClick={toggle}>toggle</button>
-          {open && (
-            <div className="toggle">{props.children}</div>
-          )}
-        </div>
-      );
+  const display = () => {
+    setOPen(!open);
+  };
+
+  return (
+    <div className="collapse">
+      {/* {if ()} */}
+      {/* <button onClick={toggle}>fermé</button> */}
+      {/* {open && (
+            <div className="toggle">{props.children}</div> */}
+
+            <div className="scrollingmenu">
+            <h2>{props.label}</h2>
+      <p onClick={display} className="icon">{open? "chevron haut" : "chevron bas"}</p>
+      </div>
+      {open && <div className="toggle">{props.children}</div>}
+    </div>
+  );
 }
 export default Collapse;
-
 
 // export default Collapse;
