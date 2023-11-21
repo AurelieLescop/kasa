1d2b6b18d09d8edbc5dc205d4adfa7b5011bfbb9
Aurelie Lescop <lescop.aurelie@live.fr>
2023-11-21 19:31:22 +0100

    finalisation page error mobile

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,18 +1,21 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { NavLink } from 'react-router-dom';
-
+import { NavLink } from "react-router-dom";
 
 function NotFound() {
-    return (
-      <div className="Notfound">
-    <Header />
-    <h1 className="Notfound__title">404</h1>
-    <p className="Notfound__info" >Oups! La page que vous demandez n'existe pas.</p>
-    <NavLink to="/" className="Link" >Retourner sur la page d’accueil</NavLink>
-    <Footer />
-  </div>
-    );
-  }
-  
-  export default NotFound;
+  return (
+    <div className="Notfound">
+      <Header />
+      <h1 className="Notfound__title">404</h1>
+      <p className="Notfound__info">
+        Oups! La page que vous demandez n'existe pas.
+      </p>
+      <div className="Link">
+        <NavLink to="/">Retourner sur la page d’accueil</NavLink>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default NotFound;
