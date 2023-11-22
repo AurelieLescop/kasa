32abc1ab5ee4f216bcfd243185e22151009b7b74
Aurelie Lescop <lescop.aurelie@live.fr>
2023-11-22 18:12:44 +0100

    page accueil tablette desktop

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,7 +3,7 @@ import coverpicture from '../IMG_cover.png';
 function Banner() {
     return (
 <div className='banner'>
-<h1>Chez vous, <br /> partout et ailleurs</h1>
+<h1>Chez vous, <br className="break"/> partout et ailleurs</h1>
 <img src={coverpicture} className="banner_img" alt="photographie de paysage" /> 
 
 </div>
