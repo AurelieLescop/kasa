8e72dba9751a06552d2b7d62fb79ca72b1eb8d2d
Aurelie Lescop <lescop.aurelie@live.fr>
2023-12-09 08:30:29 +0100

    modification fetch page Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,32 +8,39 @@ const homebannerheight = 111;
 
 function Home() {
   const [datas, setDatas] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
+    setIsLoading(true);
     fetch("logements.json")
       .then((response) => response.json())
       .then((data) => setDatas(data))
-      .catch((e) => console.error(e));
+      .catch((e) => console.error(e))
+      .finally(() => setIsLoading(false));
   }, []);
 
-  return (
-    <main className="home">
-      <Banner src={coverpicture} height={homebannerheight}>
-        <div className="bannertitle">
-          <h1>
-            Chez vous, <br className="break" />
-            partout et ailleurs
-          </h1>
-        </div>
-      </Banner>
-      <section className="accomodation">
-        <section className="accomodation__list">
-          {datas.map((data) => (
-            <Card key={data.id} data={data} />
-          ))}
+  if (isLoading) {
+    return <p>Loading...</p>;
+  } else {
+    return (
+      <main className="home">
+        <Banner src={coverpicture} height={homebannerheight}>
+          <div className="bannertitle">
+            <h1>
+              Chez vous, <br className="break" />
+              partout et ailleurs
+            </h1>
+          </div>
+        </Banner>
+        <section className="accomodation">
+          <section className="accomodation__list">
+            {datas.map((data) => (
+              <Card key={data.id} data={data} />
+            ))}
+          </section>
         </section>
-      </section>
-    </main>
-  );
+      </main>
+    );
+  }
 }
 
 export default Home;
